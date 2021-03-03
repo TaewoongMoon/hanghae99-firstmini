04c0ee87bd68d0645c9d4f3849ca6b4dc9195a43
Youn-Hwan <beadoer1@gmail.com>
2021-03-03 16:19:27 +0900

    항해99 mini project 중간 업로드
    1. HTML : CSS 디자인 업데이트, slick 을 통한 카드 넘김 효과 구현, 지역 image 저장/적용
    2. Python : 네이버 API 요청 구현 완료

--- app.py
+++ app.py
@@ -8,17 +8,29 @@ import urllib.request
 ## HTML을 주는 부분
 @app.route('/')
 def home():
-   return render_template('mypage.html')
+   return render_template('main.html')
 
-location_receive = "서울 명소"
+### main page 내 함수
+@app.route('/api/search-1', methods=['POST']) # 해당 주소와 형식으로 API 요청이 오면 아래 함수를 실행
+def search_place(): 
+   # 'location_give(클라이언트 data의 key 값)'의 value를 받아 " 명소"를 합친다.
+   search_word = request.form['region_give'] + " 명소"
+   # 위 변수를 가지고 네이버 검색 요청 함수를 실행하여 반환 값을 저장
+   result_return = search(search_word)
+   return jsonify({"places" : result_return}) # 함수의 리턴 값을 json 형태로 클라이언트에 반환
 
-# API 역할을 하는 부분
-@app.route('/review', methods=['GET'])
-def test_post1():
-   client_id = "-"
-   client_secret = "-"
-   encText = urllib.parse.quote(location_receive)
-   url = "https://openapi.naver.com/v1/search/local.json?query=" + encText + "&display=5" + "&sort=comment" # json 결과
+@app.route('/api/search-2', methods=['POST'])
+def search_accommodation(): 
+   search_word = request.form['place_give'] + " 숙소"
+   result_return = search(search_word)
+   return jsonify({"accommodations" : result_return})
+
+# 네이버 검색 요청 함수
+def search(value):
+   client_id = ""
+   client_secret = ""
+   encText = urllib.parse.quote(value) # 지역변수 value 값을 넣어 url 에 넣을 값으로 변경해줌
+   url = "https://openapi.naver.com/v1/search/local.json?query=" + encText +  "&display=5" + "&sort=comment" # json 결과
    # url = "https://openapi.naver.com/v1/search/blog.xml?query=" + encText # xml 결과
    request = urllib.request.Request(url)
    request.add_header("X-Naver-Client-Id",client_id)
@@ -27,14 +39,24 @@ def test_post1():
    rescode = response.getcode()
    if(rescode==200):
       response_body = response.read()
-      print(response_body.decode('utf-8'))
+      result = response_body.decode('utf-8') # 출력해보니 json 형태의 값
+      return result # json 형태의 결과값을 반환
    else:
       print("Error Code:" + rescode)
-   return jsonify({'result':'success', 'msg': "성공!!"})
 
-@app.route('/delete', methods=['POST'])
-def test_post2():
-   location_receive = request.form['location_give']
+
+# myPage review 저장 함수
+@app.route('/api/review', methods=['POST'])
+def save_review():
+   location_receive = request.form['data_give']
+   search_attraction(location_receive)
+   return jsonify({'result':'success', 'msg': location_receive})
+
+# myPage 여행 삭제 함수
+@app.route('/api/delete', methods=['POST'])
+def delete_travel():
+   location_receive = request.form['data_give']
+   search_attraction(location_receive)
    return jsonify({'result':'success', 'msg': location_receive})
 
 if __name__ == '__main__':  
